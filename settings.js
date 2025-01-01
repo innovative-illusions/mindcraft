beb45b54f0bb776044d4aa85ffd133d0b691e528
Ben <lightbearermusic55@gmail.com>
2025-01-01 00:32:57 -0600

    feat: update NPC profiles and add new creative mode configuration

--- settings.js
+++ settings.js
@@ -10,10 +10,11 @@ export default
     "mindserver_host": "localhost",
     "mindserver_port": 8080,
     
-    "profiles": [
-        "./custom_profiles/blue.json",
-        "./custom_profiles/red.json",
-        // "./profiles/gpt.json",
+        "profiles": [
+        // "./andy-creative.json",
+        // "./custom_profiles/blue.json",
+        // "./custom_profiles/red.json",
+        "./profiles/andy_npc.json",
         // "./profiles/claude.json",
         // "./profiles/gemini.json",
         // "./profiles/llama.json",
@@ -32,7 +33,7 @@ export default
     "allow_insecure_coding": true, // allows newAction command and model can write/run code on your computer. enable at own risk
     "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
 
-    "max_messages": 30, // max number of messages to keep in context
+    "max_messages": 50, // max number of messages to keep in context
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
     "verbose_commands": true, // show full command syntax
     "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
