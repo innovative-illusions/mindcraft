cb6ba17d4e7702f3c0135df81eae841112567b5c
Ben <lightbearermusic55@gmail.com>
2024-12-31 19:28:21 -0600

    customize the settings.js file

--- settings.js
+++ settings.js
@@ -28,10 +28,10 @@ export default
     "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
     "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
-    "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
+    "allow_insecure_coding": true, // allows newAction command and model can write/run code on your computer. enable at own risk
     "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
 
-    "max_messages": 15, // max number of messages to keep in context
+    "max_messages": 30, // max number of messages to keep in context
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
     "verbose_commands": true, // show full command syntax
     "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
